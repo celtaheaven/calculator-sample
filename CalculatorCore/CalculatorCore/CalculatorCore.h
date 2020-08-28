ae85d309e1612e3f2f2e374492d165895c40c274
Guilherme Guimarães <g.guimaraes@accenture.com>
2020-08-28 12:02:32 -0300

    Add CalculatorCore module and its files

---
+++ CalculatorCore/CalculatorCore/CalculatorCore.h
@@ -0,0 +1,19 @@
+//
+//  CalculatorCore.h
+//  CalculatorCore
+//
+//  Created by g.guimaraes on 28/08/20.
+//  Copyright © 2020 Guilherme Guimaraes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalculatorCore.
+FOUNDATION_EXPORT double CalculatorCoreVersionNumber;
+
+//! Project version string for CalculatorCore.
+FOUNDATION_EXPORT const unsigned char CalculatorCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculatorCore/PublicHeader.h>
+
+
